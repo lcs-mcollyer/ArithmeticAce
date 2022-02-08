50429b8b4d8092d53f5e5cc6b95043b2f7a34d2b
matt collyer <mcollyer@lcs.on.ca>
2022-02-08 08:47:23 -0500

    Initial Commit

---
+++ Arithmetic Ace/Arithmetic_AceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Arithmetic_AceApp.swift
+//  Arithmetic Ace
+//
+//  Created by Matthew Collyer on 2022-02-08.
+//
+
+import SwiftUI
+
+@main
+struct Arithmetic_AceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
